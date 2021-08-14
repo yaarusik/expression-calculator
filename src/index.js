17673d014fe7d4cf910b96cd9efad025c44687f2
Ruslan <yaarusik@gmail.com>
2021-08-14 21:15:25 +0500

    code refactoring

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let newExpr = expr.replace(/\*/g, ' * ');
-    newExpr = newExpr.replace(/\//g, ' / ');
-    newExpr = newExpr.replace(/\-/g, ' - ');
-    newExpr = newExpr.replace(/\+/g, ' + ');
-    newExpr = newExpr.replace(/\(/g, ' ( ');
-    newExpr = newExpr.replace(/\)/g, ' ) ');
+    //приводим в нужный вид строку
+    let newExpr = expr.replace(/\*/g, ' * ')
+    .replace(/\//g, ' / ')
+    .replace(/\-/g, ' - ')
+    .replace(/\+/g, ' + ')
+    .replace(/\(/g, ' ( ')
+    .replace(/\)/g, ' ) ');
     //убираем лишние пробелы
     newExpr =newExpr.replace(/\s +/g, ' ');
     newExpr = newExpr.trim().split(' ');
+    //проверяем скобки
     checkBrackets(newExpr);
 
     const OPERATORS = ['+', "-", "*", "/", '('];
@@ -24,40 +26,40 @@ function expressionCalculator(expr) {
         '/': 2,
         '(': 0
     }
+
     let stack = [];
     let outputString = [];
    
     function checkBrackets(expr) {
-        const leftBracket = expr.filter(el => el == '(').length
-        const rightBracket = expr.filter(el => el == ')').length
+        let leftBrackets = expr.filter(el => el == '(').length;
+        let rightBrackets = expr.filter(el => el == ')').length;
       
-        if (leftBracket != rightBracket) {
+        if (leftBrackets != rightBrackets) {
           throw new Error ('ExpressionError: Brackets must be paired')
         }
       }
 
 
-    function variables(){
+    function mathOperation(){
         let last = outputString.pop();
         let penultimate = outputString.pop();
         let symbol = stack.pop();
         //вычислили
         let mathOperation = operation(symbol, last, penultimate);
         //добавили результат
-        
         outputString.push(+mathOperation);
     }
 
-    function operation(symbol, a, b){
-        if ( b / a == Infinity) {
+    function operation(symbol, a, b){     
+        if ( symbol == '/' && (b / a == Infinity) ) {
             throw new Error("TypeError: Division by zero.");
         }
        return symbol == '-' ? b - a : symbol == '+' ? b + a : symbol == '/' ? b / a : b * a;
     }
     //итоговый результат
-    function calculateResult(){
+    function toCalculate(){
         while (stack.length){
-            variables();
+            mathOperation();
         }
     }
 
@@ -67,38 +69,35 @@ function expressionCalculator(expr) {
             //проверка на пустоту
             if (!stack.length) {
                 stack.push(item);
-                //проверка на приоритет
+                
             } else if(item == '('){
                 stack.push(item);
-            } else if( OPERATORS__PRIORITY[stack[stack.length - 1]] < OPERATORS__PRIORITY[item]) {
-                stack.push(item);
+            } //проверка на приоритет 
+            else if( !(OPERATORS__PRIORITY[stack[stack.length - 1]] < OPERATORS__PRIORITY[item])) {
+                // одинаковые знаки операцйии не должны стоять рядом
+                while(stack.length && !(OPERATORS__PRIORITY[stack[stack.length - 1]] < OPERATORS__PRIORITY[item]) ){
+                    mathOperation();
+                }
+                stack.push(item);                
             } //если приоритет ниже берем два последних элемента из выходной строки и вычисляем
             else {
-               variables();
-                // одинаковые знаки операцйии не должны стоять рядом
                 stack.push(item);
-            }
-            
+            }          
         } else if (item == ')') {
             //вычисляем пока не дойдем до ближайшей скобки
             while(stack[stack.length - 1] !== '('){
-                variables()
+                mathOperation()
             }
             stack.pop();
         } else {
             outputString.push(+item);
-
         }
     });
 
-    calculateResult();
+    toCalculate();
 
-    //console.log(stack);
-    //console.log(outputString);
     return +outputString[0];  
 }
-
-//console.log(expressionCalculator(" 20 - 57 * 12 - (  58 + 84 * 32 / 27  ) "));
 
  module.exports = {
     expressionCalculator,
